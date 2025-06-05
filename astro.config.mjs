0d023db10920145c5d7315d6ee2f5f5b07178e77
Gunnar Torfi <gunnartorfis@gmail.com>
2025-06-05 22:16:06 +0000

    chore: github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gunnartorfis.github.io",
-  base: "/cv",
   integrations: [],
   output: "static",
+
   vite: {
     resolve: {
       alias: {
