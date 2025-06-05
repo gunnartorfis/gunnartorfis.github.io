98f74badfb490164c0e5125ec41e99f001474a47
Gunnar Torfi <gunnartorfis@gmail.com>
2025-06-05 23:20:51 +0000

    chore: typos

--- src/data/resume-data.ts
+++ src/data/resume-data.ts
@@ -5,11 +5,11 @@ export const RESUME_DATA = {
   locationLink: "https://www.google.com/maps/place/Kópavogur",
   about:
     "Husband, father of two, and a software enthusiast building with React and React Native.",
-  summary: `Steering the technological helm at Noona, I've been instrumental in leveraging React Native to craft innovative mobile experiences for many years. As the former CTO of Noona, I devoted more than seven years to shaping our technology vision and ensuring our native and web applications delivered high-quality, scalable applications.
+  summary: `Steering the technological helm at Noona, I've been instrumental in leveraging React Native to craft innovative mobile experiences for many years. As the former CTO of Noona, I devoted more than seven years to shaping our technology vision and ensuring our native and web apps were at par with the best in the industry.
 
-As a co-founder, my responsibilities spanned multiple domains including product development, engineering, SEO optimization, and CI/CD pipeline management. I've also had to learn a lot about the business side of things, working closely with the CEO and the rest of the team.
+As a co-founder, my responsibilities spanned multiple domains including product development, engineering, SEO optimization, DevOps, and CI/CD pipeline management. I've also had to learn a lot about the business side of things, working closely with the CEO and the rest of the team.
 
-In later years, I've also been working more and more in open source, making contributions to Expo and other React Native libraries. Primarily through my own library, <a href="https://github.com/gunnartorfis/sonner-native" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">Sonner Native</a>, a React Native port of the popular web toast library Sonner.`,
+In later years, I've also been contributing back to the community by making contributions to Expo and other React Native libraries, primarily through my own library, <a href="https://github.com/gunnartorfis/sonner-native" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">Sonner Native</a>, a React Native port of the popular web toast library Sonner.`,
   contact: {
     email: "gunnartorfis@gmail.com",
     tel: "+3548653230",
@@ -34,7 +34,7 @@ In later years, I've also been working more and more in open source, making cont
   education: [
     {
       school: "Reykjavík University",
-      degree: "Bachelor's Degree in Software Engineering",
+      degree: "Bachelor's degree in Software Engineering",
       start: "2015",
       end: "2018",
     },
@@ -54,16 +54,14 @@ In later years, I've also been working more and more in open source, making cont
       title: "Co-Founder, CTO, Tech Lead",
       start: "2018",
       end: null,
-      description: `<p>Over the years, I have contributed to various aspects of our technology stack, being the first developer at Noona. My main focus for the first months was building the marketplace from the ground-up (the consumer-facing apps). I initially developed a client-side SPA using Create React App back in 2018 and a mobile app with bare React Native, all written in Javascript, powered by a GraphQL API.<br/><br/>Through iterative improvements, I migrated the codebase to <strong>Typescript</strong>, <strong>Next.js</strong>, <strong>Expo</strong>, <strong>Nativewind</strong> and <strong>Tailwind</strong>, things were in a good place. To support team growth and development velocity, I architected a more scalable solution that enabled parallel development across multiple projects.</p>
-
-<p>I also started working on the backend app (back-office for salon owners), Noona HQ, which was a Meteor app. I gradually migrated it to React + REST.<br/><br/>That was when I moved our Expo app and Next.js app to a <strong>monorepo</strong>, using <strong>Turborepo</strong> and pnpm workspaces. This enabled code sharing between the mobile apps and the web app. We weren't only sharing some UI components but entire features/screens. This meant we still benefited from Next.js's data fetching capabilities, but not having to write everything both for the web and native. This also bridged the gap between the web and native development, improving feature parity between the platforms.</p>
+      description: `<That>Over the years, I have contributed to various aspects of our technology stack, being the first developer at Noona. My main focus to begin with was building the marketplace (the consumer-facing apps) from the ground-up. I initially developed a client-side SPA using Create React App back in 2018 and a mobile app with bare React Native, all written in Javascript, powered by a GraphQL API.<br/><br/>Through iterative improvements, I migrated the codebases to <strong>Typescript</strong>, <strong>Next.js</strong>, <strong>Expo</strong>, <strong>Nativewind</strong> and <strong>Tailwind</strong>, and things were in a good place. To further support team growth and development velocity, I architected a more scalable solution that enabled parallel development across multiple projects, leveraging React Native for Web. That was when I moved our Expo app and Next.js app to a <strong>monorepo</strong>, using <strong>Turborepo</strong> and pnpm workspaces. This enabled code sharing between the mobile apps and the web app. We weren't only sharing UI components, but entire features/screens. This meant we still benefited from Next.js's data fetching capabilities, etc., but didn't have to write everything twice, for the web and native. This also bridged the gap between the web and native development, improving feature parity between the platforms.</p>
 
 <br />
-<p>Now for Noona HQ, the enormous back-office app that company owners use. I made several significant contributions to that product; rewrite from JS to TS, leading the development of a fully-fledged POS system, initiating a big rewrite/transition from Meteor to React. What I am proud of the most in the HQ development is that I architected and implemented multiple calendar systems including a traditional appointment calendar for salons and a timeline-based reservation system for restaurants, both featuring drag-and-drop functionality, resizing capabilities, and optimistic updates for real-time user feedback.</p>
+<p>For Noona HQ, the enormous back-office app that company owners use. I made several significant contributions to that product; rewrite from JS to TS, leading the development of a fully-fledged POS system, initiating a big rewrite/transition from Meteor to React and introducing shadcn/ui. What I am proud of the most in the HQ development is that I architected and implemented multiple calendar systems including a traditional appointment calendar for salons and a timeline-based reservation system for restaurants, both featuring drag-and-drop functionality, resizing capabilities, and optimistic updates for instant user feedback.</p>
 <br />
-<p>For the past couple of months I have been part of creating a new AI-powered application. Since it is a greenfield project, I had the opportunity to work on a lot of new technologies, including <strong>Tanstack Start</strong>, <strong>Python for LLM backend systems</strong>, and <strong>Vercel's AI SDK</strong>. This includes handling streaming with server-sent events and building highly interactive offline-first applications.</p>
+<p>For the past couple of months I have also been part of creating a new AI-first application. Since it is a greenfield project, I had the opportunity to work on a lot of new technologies, including <strong>Tanstack Start</strong>, <strong>Python for LLM backend systems</strong>, and <strong>Vercel's AI SDK</strong>. This includes handling streaming with server-sent events and building highly interactive offline-first applications.</p>
 <br />
-<p>To keep things short, I want to mention a few other things I've been working on for the last couple of years:</p>
+<p>To keep things short, I also briefly want to mention a few other things I've been working on for the last couple of years:</p>
 
 <ul class="list-inside list-disc">
 <li>Next.js app router</li>
